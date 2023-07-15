c4e71743feb65c1d81165ffd6098b87691889175
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-07-15 22:04:16 +0200

    add missing handleclick

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -14,7 +14,8 @@ const CustomButton = ({ type, title, customStyles, handleClick }) => {
   return (
     <button
      className={`px-2 py-1.5 flex-1 rounded-md $ ${customStyles}`}
-    style={generateStyle(type)}
+     style={generateStyle(type)}
+     onClick={handleClick}
     >
       {title}
     </button>
