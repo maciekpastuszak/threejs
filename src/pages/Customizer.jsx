cd3992a261a4242844f162be4b02f6573b3d5ae7
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-07-16 11:02:00 +0200

    typo

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -27,7 +27,7 @@ const Customizer = () => {
                   <Tab 
                     key={tab.name}
                     tab={tab}
-                    handleClick={() => ()}
+                    handleClick={() => {}}
                   />
                 ))}
               </div>
