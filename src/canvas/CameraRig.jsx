80e047225bc9f1c43a75c6a87cc450577a70b51b
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-07-17 14:38:46 +0200

    add useFrame to CameraRig

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -9,10 +9,20 @@ const CameraRig = ({ children }) => {
   const group = useRef();
   const snap = useSnapshot(state);
 
+  useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+  })
+
+  //set the initial positiin of the model
+  
+
   // set the model rotation smoothly
   easing.dampE(
     group.current.rotation,
-    [state.pointer.y / 10, -state.pointer.x / 5,0]
+    [state.pointer.y / 10, -state.pointer.x / 5,0],
+    0.25,
+    delta
   )
 
 
