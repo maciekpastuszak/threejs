4899182b98732340b4a779b4ff1db2e3e7217ceb
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-07-17 13:59:44 +0200

    add consts to Shirt

--- src/canvas/Shirt.jsx
+++ src/canvas/Shirt.jsx
@@ -7,6 +7,11 @@ import { Decal, useGLTF, useTexture } from '@react-three/drei';
 import state from '../store';
 
 const Shirt = () => {
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF('/shirt_baked.glb')
+
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
   return (
     <div>Shirt</div>
   )
