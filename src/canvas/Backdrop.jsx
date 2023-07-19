bb194e50b283eb93a14675333fea8eac7ed908a2
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-07-19 10:45:12 +0200

    add Accumulative shadow parameters

--- src/canvas/Backdrop.jsx
+++ src/canvas/Backdrop.jsx
@@ -5,8 +5,16 @@ import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
 
 
 const Backdrop = () => {
+  const shadows = useRef();
+
   return (
     <AccumulativeShadows
+      ref={shadows}
+      temporal
+      frames={60}
+      alphaTest={0.85}
+      scale={10}
+      rotation={[Math.PI / 2, 0, 0]}
       position={[0, 0, -0.14]}
     >
       <RandomizedLight 
