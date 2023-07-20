75f92445aa58564024f94c001c4cba1e3f5fc175
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-07-20 12:53:15 +0200

    add filepicker params and handleDecals

--- src/pages/Customizer.jsx
+++ src/pages/Customizer.jsx
@@ -30,13 +30,36 @@ const generateTabContent = () => {
     case "colorpicker":
       return <ColorPicker />
     case "filepicker":
-      return <FilePicker />
+      return <FilePicker
+      file={file}
+      setFile={setFile}
+      readFile={readFile}
+    />
     case "aipicker":
       return <AIPicker/>
     default:
       return null;
+    }
   }
-}
+
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type];
+
+    state[decalType.stateProperty] = result;
+
+    if(!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab)
+    }
+  }
+
+  const readFile = (type) => {
+    reader(file)
+    .then((result) => {
+      handleDecals(type, result);
+      setActiveEditorTab('')
+    })
+  }
+
 
   return (
     <AnimatePresence>
