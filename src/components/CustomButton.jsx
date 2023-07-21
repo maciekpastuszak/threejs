b9fb92b1a6aacf7733bc392f71834453fa004df1
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-07-21 20:46:48 +0200

    bugfix

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSnapshot } from 'valtio';
 
+import state from '../store';
 import { getContrastingColor } from '../config/helpers';
 
 const CustomButton = ({ type, title, customStyles, handleClick }) => {
