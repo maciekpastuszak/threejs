aa668efae367a420f2b14c65b7d58cf3b88b9be6
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-07-21 16:18:39 +0200

    add inpput in FilePicker

--- src/components/FilePicker.jsx
+++ src/components/FilePicker.jsx
@@ -5,7 +5,17 @@ import CustomButton from './CustomButton'
 const FilePicker = ({ file, setFile, readFile }) => {
   return (
     <div className="filepicker-container">
-      
+       <div className="flex-1 flex flex-col">
+        <input 
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <label htmlFor="file-upload" className="filepicker-label">
+          Upload File
+        </label>
+      </div>
     </div>
   )
 }
