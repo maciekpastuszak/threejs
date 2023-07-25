750860a4f875e6555563b14021c0bbd80161a941
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-07-25 09:49:27 +0200

    add textarea props

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -5,7 +5,11 @@ import CustomButton from './CustomButton';
 const AIPicker = ({ prompt, setPrompt, generatingImg, handleSubmit }) => {
   return (
     <div className="aipicker-container">
-      <textarea 
+      <textarea
+        placeholder="Ask AI..." 
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
         className="aipicker-textarea"
       />
     </div>
