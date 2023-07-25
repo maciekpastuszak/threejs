ca0b3d0a0375e7e086b6bcf873b0024894408b09
Maciej Pastuszak <maciej.pastuszak@plte.pl>
2023-07-25 09:38:18 +0200

    add post route for prompting images

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -10,8 +10,26 @@ const config = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+const openai = new OpenAIApi(config);
+
 router.route('/').get((req, res) => {
     res.status(200).json({ message: "Hello from DALL.E ROUTES" })
+})
+
+router.route('/').post(async (req, res) => {
+    try { 
+        const { prompt } = req.body;
+
+        const response = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json'
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Something went wrong" })
+    }
 })
 
 export default router;
